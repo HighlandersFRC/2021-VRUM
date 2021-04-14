45465b59d6149d2cbc23e8f80be1995f5530861c
jacob6838 <51723791+jacob6838@users.noreply.github.com>
2021-04-13 18:07:21 -0600

    creating initial map

--- vrum_mobile/lib/main.dart
+++ vrum_mobile/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:background_location/background_location.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:vrum_mobile/Location.dart';
 import 'package:vrum_mobile/generatePSM.dart';
 import 'package:vrum_mobile/getPSM.dart';
@@ -10,6 +11,7 @@ import 'package:vrum_mobile/getPSM.dart';
 void main() {
   runApp(MaterialApp(
     title: 'Navigation Basics',
+    // home: MapSample(),
     home: FirstRoute(),
   ));
 }
@@ -50,6 +52,49 @@ class FirstRoute extends StatelessWidget {
       ),
       ),
     );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
 
@@ -127,12 +172,13 @@ class CarRoute extends StatelessWidget {
 
         Center(
           child: Column (
-            children: [ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Back'),
-            ),
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Back'),
+              ),
               StreamBuilder<Location>(
                   stream: locationStream,
                   builder: (context, snapshot) {
@@ -153,6 +199,17 @@ class CarRoute extends StatelessWidget {
                   getPSM(locationStream);
                   allowLocation = true;},
                 child: Text("Get Location"),
+              ),
+              Container(
+                height: 600,
+                child: GoogleMap(
+                  mapType: MapType.normal,
+                  initialCameraPosition: CameraPosition(
+                      bearing: 180,
+                      target: LatLng(47, -105),
+                      zoom: 15
+                  ),
+                ),
               ),
             ],
           )
