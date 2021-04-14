d50c0985729ab27f4963101bd80bf27b1c68a90f
Debbie English <debbie@neaeraconsulting.com>
2021-04-13 18:20:26 -0600

    integrated theme into project

--- vrum_mobile/lib/main.dart
+++ vrum_mobile/lib/main.dart
@@ -3,171 +3,56 @@ import 'dart:io';
 
 import 'package:background_location/background_location.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:vrum_mobile/Location.dart';
 import 'package:vrum_mobile/generatePSM.dart';
 import 'package:vrum_mobile/getPSM.dart';
+import 'package:vrum_mobile/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:vrum_mobile/navigation_home_screen.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'Navigation Basics',
-    home: FirstRoute(),
-  ));
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(<DeviceOrientation>[DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_) => runApp(MyApp()));
 }
-
 bool allowLocation = false;
 final LocationProvider locationProvider = LocationProvider();
 final locationStream = locationProvider.locationStream;
-class FirstRoute extends StatelessWidget {
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Platform.isAndroid ? Brightness.dark : Brightness.light,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarDividerColor: Colors.grey,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+    return MaterialApp(
+      title: 'Welcome to VRUM',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: AppTheme.textTheme,
+        platform: TargetPlatform.iOS,
       ),
-      body: Center(
-        child: Column (
-        children: <Widget> [
-          ElevatedButton(
-            child: Text('Pedestrian'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => PedestrianRoute()),
-              );
-            },
-          ),
-          ElevatedButton(
-            child: Text('Vehicle'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => CarRoute()),
-              );
-            },
-          ),
-        ],
-      ),
-      ),
+      home: NavigationHomeScreen(),
     );
   }
 }
 
-class PedestrianRoute extends StatelessWidget {
-  void sendPSM() async {
-    print('Sent PSM');
-  }
-  // void main() {
-  //   const time = const Duration(seconds: 30);
-  //   Timer.periodic(time, (Timer timer) {
-  //     sendPSM();
-  //   });
-  // }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Pedestrian"),
-      ),
-      body: Column(
-        children: <Widget>[
-        Center(
-          child: Column (
-            children: [ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Back'),
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
 
-            ),
-              // var time = const Duration(//milliseconds: // // seconds: // // minutes: // ...);
-              //   Timer.periodic(time, (timer) => {sentPSM()}
-              // ),
-              StreamBuilder<Location>(
-                  stream: locationStream,
-                  builder: (context, snapshot) {
-                    if(allowLocation) {
-                      return Text(
-                        "Latitude: ${snapshot.data?.latitude}, Latitude: ${snapshot.data?.longitude}",
-                      );
-                    }
-                    else {
-                      return Text(
-                        "Latitude: null, Longitude: null",
-                      );
-                    }
-                  }
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  allowLocation = true;
-                  generatePSM(locationStream);
-                },
-                child: Text("Get Location"),
-              ),
-            ],
-          )
-      ),
-      ],
-    ),
-    );
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
   }
 }
-
-class CarRoute extends StatelessWidget {
-  // UpdateTextState createState() => UpdateTextState();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Vehicle"),
-      ),
-      body: Column(
-      children: <Widget>[
-
-        Center(
-          child: Column (
-            children: [ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Back'),
-            ),
-              StreamBuilder<Location>(
-                  stream: locationStream,
-                  builder: (context, snapshot) {
-                    if(allowLocation) {
-                      return Text(
-                        "Latitude: ${snapshot.data?.latitude}, Latitude: ${snapshot.data?.longitude}",
-                      );
-                    }
-                    else {
-                      return Text(
-                          "Latitude: null, Longitude: null",
-                      );
-                    }
-                  }
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  getPSM(locationStream);
-                  allowLocation = true;},
-                child: Text("Get Location"),
-              ),
-            ],
-          )
-        ),
-      ],
-    ),
-    );
-
-  }
-
-  // @override
-  // State<StatefulWidget> createState() {
-  //   // TODO: implement createState
-  //   throw UnimplementedError();
-  // }
-}
-
-
